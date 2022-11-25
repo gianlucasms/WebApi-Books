4a70a3a2e35d15949a7482e9f12ee18a940e4a7e
Gian <gian.silva@sixconsult.com.br>
2022-11-25 12:15:13 -0300

    WebAPIBooks

---
+++ WebApi/Model/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Model
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ WebApi/Repositories/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApi.Model;
+
+namespace WebApi.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(int Id);
+        Task<Book> Create(Book book);
+        Task Update(Book book);
+        Task Delete(int Id);
+    }
+}
